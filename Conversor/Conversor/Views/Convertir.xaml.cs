f53b6ac2f1056a257e467368a253c5788f20e93f
ARodriguez1714 <jantoniochavezr14@gmail.com>
2024-01-22 18:51:38 -0600

    Add project files.

---
+++ Conversor/Conversor/Views/Convertir.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Convertir : ContentPage
+    {
+        double cm, m;
+        public Convertir()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular()
+        {
+            cm = Convert.ToDouble(txtcm.Text);
+            m = cm / 100;
+        }
+
+        private void btnVolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
